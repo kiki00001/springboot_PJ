2d6e09ad11f80e14ccc4140223e7044914d95f7d
ahn <kiki_0001@naver.com>
2022-04-12 22:36:15 +0900

    test

--- keke_land/src/main/java/com/keke_land/member/domain/Member.java
+++ keke_land/src/main/java/com/keke_land/member/domain/Member.java
@@ -1,17 +1,19 @@
 package com.keke_land.member.domain;
 
 import lombok.Getter;
+import lombok.Setter;
 
 
 import javax.persistence.*;
 
 @Entity
 @Getter
+@Setter
 @Table(name ="member")
 public class Member {
     @Id
     @GeneratedValue(strategy= GenerationType.IDENTITY)
-    private Long user_num;
+//    private Long user_num;
     private String id;
     private String pw;
     private String name;
