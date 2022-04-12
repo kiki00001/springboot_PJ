2fc77f38a4fa962a4453a81fd01f493d4965d57f
ahn <kiki_0001@naver.com>
2022-04-12 22:20:09 +0900

    update

--- keke_land/src/main/java/com/keke_land/main/controller/HomeController.java
+++ keke_land/src/main/java/com/keke_land/main/controller/HomeController.java
@@ -1,2 +1,34 @@
-package com.keke_land.main.controller;public class HomeController {
+package com.keke_land.main.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+
+    @RequestMapping("/")
+    public String main() {
+        return "view/main";
+    }
+    @RequestMapping("/home")
+    public String home() {
+        return "view/main";
+    }
+
+    @RequestMapping("/board")
+    public String board() {
+        return "view/board";
+    }
+    @RequestMapping("/test1")
+    public String test1() {
+        return "view/test1";
+    }
+    @RequestMapping("/test2")
+    public String test2() {
+        return "view/test2";
+    }
+
+
+
 }
---
+++ keke_land/src/main/java/com/keke_land/member/repository/JpaMemberRepository.java
@@ -0,0 +1,2 @@
+package com.keke_land.member.repository;public interface JpaMemberRepository {
+}
