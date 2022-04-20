bb1777b1312a8c1abf1c86f1b701026ae45d09b5
ahn <kiki_0001@naver.com>
2022-04-20 23:11:15 +0900

    update

--- keke_land/src/main/java/com/keke_land/member/domain/Member.java
+++ keke_land/src/main/java/com/keke_land/member/domain/Member.java
@@ -5,6 +5,7 @@ import lombok.Setter;
 
 
 import javax.persistence.*;
+import java.util.Objects;
 
 @Entity
 @Getter
@@ -13,11 +14,27 @@ import javax.persistence.*;
 public class Member {
     @Id
     @GeneratedValue(strategy= GenerationType.IDENTITY)
-//    private Long user_num;
+    private Long user_num;
     private String id;
     private String pw;
     private String name;
     private String email;
 
 
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Member member = (Member) o;
+        return id.equals(member.id) &&
+                name.equals(member.name) &&
+                email.equals(member.email) &&
+                pw.equals(member.pw);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, email, pw);
+    }
 }
