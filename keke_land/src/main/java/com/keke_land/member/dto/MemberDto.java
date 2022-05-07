8218535558bf1bd9392ee5e26c63607fb0f27684
ahn <kiki_0001@naver.com>
2022-05-07 22:29:45 +0900

    update

--- keke_land/src/main/java/com/keke_land/member/dto/MemberDto.java
+++ keke_land/src/main/java/com/keke_land/member/dto/MemberDto.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Entity;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
@@ -22,10 +23,26 @@ public class MemberDto {
     @NotBlank(message = "pw를 입력하세요")
     @Pattern(regexp="(?=.*[0-9])(?=.*[a-z])(?=.*\\W)(?=\\S+$).{8,16}",
             message = "8~16 characters consisting of letters(A-Z, a-z), numbers, or special characters.")
-    private String pw;
+    private String password1;
 
+    @NotBlank(message = "pw를 입력하세요")
+    @Pattern(regexp="(?=.*[0-9])(?=.*[a-z])(?=.*\\W)(?=\\S+$).{8,16}",
+            message = "8~16 characters consisting of letters(A-Z, a-z), numbers, or special characters.")
+    private String password2;
+/*
     @Email(message = "이메일 형식에 맞지 않습니다.")
     private String email;
 
+    @NotBlank(message = "name를 입력하세요")
     private String name;
+
+    public MemberDto(String id, String password1, String password2, String email, String name){
+        this.id = id;
+        this.password1 = password1;
+        this.password2 = password2;
+        this.email = email;
+        this.name = name;
+    }
+    */
+
 }
--- keke_land/src/main/java/com/keke_land/member/service/MemberService.java
+++ keke_land/src/main/java/com/keke_land/member/service/MemberService.java
@@ -1,8 +1,45 @@
 package com.keke_land.member.service;
 
+import com.keke_land.member.domain.Member;
+import com.keke_land.member.repository.JpaMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.validation.Errors;
+import org.springframework.validation.FieldError;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class MemberService {
 
+    @Autowired
+    JpaMemberRepository JpaMemberRepository;
+
+/*
+    // 회원가입 시, 유효성 체크
+    public Map<String, String> validateHandling(Errors errors) {
+        Map<String, String> validatorResult = new HashMap<>();
+
+        for (FieldError error : errors.getFieldErrors()) {
+            String validKeyName = String.format("valid_%s", error.getField());
+            validatorResult.put(validKeyName, error.getDefaultMessage());
+        }
+
+        return validatorResult;
+    }
+*/
+
+    //회원가입
+    public void registration(Member member){
+
+        JpaMemberRepository.save(member);
+
+
+    }
+
+    public void test(Member member){
+        System.out.println(member.getName());
+
+    }
 }
